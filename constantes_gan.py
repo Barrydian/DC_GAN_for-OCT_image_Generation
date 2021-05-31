9670cbd05dc7f33370ce61461de3d6c19753a174
Sarry Laurent <laurent.sarry@uca.fr>
2021-05-31 14:34:55 +0200

    Compatibility with gray level images Better management of constant file

--- constantes_gan.py
+++ constantes_gan.py
@@ -1,8 +1,8 @@
 
 # Images dimension
-IMG_HEIGHT = 256
-IMG_WIDTH = 256
-IMG_CHANNEL = 3 ## Change 3 to 1 for grayscale.
+IMG_HEIGHT = 512
+IMG_WIDTH = 512
+IMG_CHANNEL = 1 ## Change 3 to 1 for grayscale.
 
 
 import tensorflow as tf
@@ -10,9 +10,9 @@ w_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
 
 ## Hyperparameters
-batch_size = 5
+batch_size = 128
 latent_dim = 128
-num_epochs = 30
+num_epochs = 2000
 n_samples = 25     ## n should always be a square of an integer.
 
 
